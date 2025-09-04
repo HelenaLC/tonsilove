cba3dd5b54296f317ab224b26f20358389ac6ca6
HelenaLC <helena@crowell.eu>
2025-09-04 16:46:13 +0200

    rmv old code

--- code/_md.R
+++
@@ -1,7 +0,0 @@
-sce <- readRDS("outs/fil-A2.rds")
-ctx <- readRDS("outs/cty-A2.rds")
-ist <- readRDS("outs/lv1-A2.rds")$clust
-sce$ist <- ist[match(colnames(sce), names(ist))]
-sce$ctx <- ctx$ctx[match(colnames(sce), colnames(ctx))]
-df <- data.frame(cell_ID=sce$cell, colData(sce))
-write.table(df, "imgs/S2/metadata.csv", sep=",", col.names=TRUE, row.names=FALSE, quote=FALSE)
